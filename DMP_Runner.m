0e54160f03bf5c79019991ac63f832608b6b6af0
Gunnar Horve <gunnarhorve@gmail.com>
2017-10-27 17:59:49 -0400

    commented step method

--- DMP_Runner.m
+++ DMP_Runner.m
@@ -1,35 +1,50 @@
-classdef DMP_Runner  
+classdef DMP_Runner < handle 
    properties
       w;    % canonical function weights (weights of gaussians)
       D;    % 1/sigma^2 (inverse variance squared of gaussians)
       c;    % canonical function spacing (means of gaussians)
       
+      g  = 0;       % progress towards goal (current goal state)
+      gd = 0;       % change in goal position
+      G  = 1;       % Goal position of DMP
+      
+      % state variables
+      x = 1;
+      v = 0;
+      z = 0;      
+      y = 0;
       vd = 0;
       xd = 0;
       zd = 0;
-      gd = 0;
       
-      % think about these
-      g = 0;
-      G = 1;
-      
-      x = 1;
-      v = 0;
-      z = 0;
-      % think about these
-      
-      y = 0;
-      yd = 0;
-      ydd = 0;
    end
    methods
+       % initialize the DMP_Runner object
+       % Inputs:
+       %    w: vector of weights
+       %    D: vector of 1/sigma^2
+       %    c: vector of canonical function values
        function obj = DMP_Runner(w, D, c)
           obj.w = w;
           obj.D = D;
           obj.c = c;
        end
+       
+       % Changes the goal position of the DMP
+       % Inputs:
+       %    G: goal position
+       function setGoal(obj, G)
+          obj.G = G;
+       end
+       
+       % Generates one time-step of a DMP trajectory
+       % Inputs:
+       %    tau: time constant
+       %    dt : integration constant
+       % Outputs:
+       %    [y yd ydd]: current DMP trajectory
        function [y yd ydd] = step(obj, tau, dt)
-          tau = 1/tau; %because ???
+          tau = 1/tau; % ????????
           % the time constants for chosen for critical damping
           alpha_z = 25;
           beta_z  = alpha_z/4;
@@ -45,8 +60,8 @@ classdef DMP_Runner
           obj.xd = obj.v*tau;
           
           obj.zd = (alpha_z*(beta_z*(obj.g-obj.y)-obj.z)+f)*tau;
-          obj.yd = obj.z*tau;
-          obj.ydd= obj.zd*tau;
+          yd = obj.z*tau;
+          ydd= obj.zd*tau;
           
           obj.gd = alpha_g*(obj.G-obj.g);
 
@@ -54,16 +69,12 @@ classdef DMP_Runner
           obj.v  = obj.vd*dt+obj.v;
 
           obj.z  = obj.zd*dt+obj.z;
-          obj.y  = obj.yd*dt+obj.y;
+          obj.y  = yd*dt+obj.y;
 
           obj.g  = obj.gd*dt+obj.g;
           
           % return stuff here
           y = obj.y;
-          yd = obj.yd;
-          ydd = obj.ydd;
        end
    end
 end
-% add back in functional scaling if you want it
-% add back in 0th order canonical function if you want it
--- testDMPSave.m
+++ testDMPSave.m
@@ -4,6 +4,7 @@ clc; clear; close;
 name = 'test.xml';
 n_rfs = 6;
 dt  = 0.001;
+
 % generating y = t^3 trajectory
 t = 0:dt:1;
 y = t.^3;
@@ -18,21 +19,21 @@ tau = 1.0;
 [w, D, c] = train_dmp(name, n_rfs, T, dt);
 % dcp('reset_state',ID,start);
 % dcp('set_goal',ID,goal,1); %the "1" resets y0 and x
+
 myRunner = DMP_Runner(w, D, c);
-
 for i=0:tau/dt
-if(i == 50)
-   'hi' 
+  Y(i+1,:) = myRunner.step(tau, dt);
 end
-
-
-
-  [y, yd, ydd] = myRunner.step(tau, dt);
-  Y(i+1,:) = [y; yd; ydd];
-end
-% YOUR TEST HERE %
 
 % plot position vs. target
 time = (0:dt:tau)';
 plot((0:dt:tau)',[Y(:,1) T(:,1)]);
 aa=axis; axis([min(time) max(time) aa(3:4)]);
+% YOUR TEST HERE %
+
+% TODO's:
+% figure out how the set the start position
+% read/write to/from XML
+% add back in functional scaling if you want it
+% add back in 0th order canonical function if you want it
+% invert 1/tau if possible
