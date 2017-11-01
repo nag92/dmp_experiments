e2573cca9ac7652ebfbe0bf701f37a915ea33d7e
Gunnar Horve <gunnarhorve@gmail.com>
2017-11-01 15:43:50 -0400

    updated to Pep 8 standards

--- Python/DMP_runner.py
+++ Python/DMP_runner.py
@@ -3,16 +3,20 @@ import numpy as np
 import re
 
 class DMP_runner():
-       #     filename: full path to .xml file containing trained DMP
-       #    start   : desired starting coordinate of DMP
-       #    goal    : desired ending coordinate of DMP
+    """
+        Initialization of DMP_runner class.
+            Inputs:
+                filename: full path to .xml file containing trained DMP
+                start   : desired starting coordinate of DMP
+                goal    : desired ending coordinate of DMP
+    """
     def __init__(self,file_name,start,goal):
         readInXML(self, file_name)
-        self.g = 0 #progress towards goal (current goal state)
-        self.gd = 0 #change in goal position
-        self.G = 1 #Goal position of DMP
+        self.g = 0   # progress towards goal (current goal state)
+        self.gd = 0  # change in goal position
+        self.G = 1   # Goal position of DMP
         
-        #state variables
+        # state variables
         self.x = 1
         self.v = 0
         self.z = 0      
@@ -20,69 +24,75 @@ class DMP_runner():
         self.vd = 0
         self.xd = 0
         self.zd = 0
-        #self.start = start
-        #self.goal = goal
         self.flag = 0
         
         self.setStart(start)
         self.setGoal(goal,1)
 
-       # Generates one time-step of a DMP trajectory
-       # Inputs:
-       #    tau: time constant
-       #    dt : integration constant
-       # Outputs:
-       #    [y yd ydd]: current DMP trajectory
-    
+    """
+        Generates one time-step of a DMP trajectory
+            Inputs:
+                tau: time constant
+                dt : integration time constant
+            Outputs:
+                y  : current 0th derivative DMP trajectory
+                yd : current 1st derivative DMP trajectory
+                ydd: current 2nd derivative DMP trajectory
+    """
     def step(self,tau_old,dt):
         
         tau = np.divide(float(1),tau_old)
         alpha_z = 25
-        beta_z  = np.divide(alpha_z,float(4))
+        beta_z = np.divide(alpha_z,float(4))
         alpha_g = np.divide(alpha_z,float(2))
         alpha_v = alpha_z
-        beta_v  = beta_z      
+        beta_v = beta_z
         
         psi = np.exp(np.multiply(-0.5,np.multiply(np.power((np.array(self.c)-self.x),2),np.array(self.D))))
         
-        print 
         amp = self.s
         In = self.v
 
-        f  = np.divide(np.sum(np.dot(np.dot(In,(self.w)),psi)), np.multiply(np.sum(psi+1.e-10),amp))
+        f = np.divide(np.sum(np.dot(np.dot(In,self.w),psi)), np.multiply(np.sum(psi+1.e-10),amp))
 
         self.vd = np.multiply(np.multiply(alpha_v,(np.multiply(beta_v,(0-self.x))-self.v)),tau)
         
         self.xd = np.multiply(self.v,tau)
         
-        #PROBLEM HERE
-        #obj.zd = (alpha_z*(beta_z*A-obj.z)+f)*tau;
+        # PROBLEM HERE
+        # obj.zd = (alpha_z*(beta_z*A-obj.z)+f)*tau;
         self.zd = np.multiply((np.multiply(alpha_z,(np.multiply(beta_z,self.g-self.y)-self.z))+f),tau)
         yd = np.multiply(self.z,tau)
         ydd= np.multiply(self.zd,tau)
         
         self.gd = np.multiply(alpha_g,(self.G-self.g))
         
-        self.x  = np.multiply(self.xd,dt)+self.x
-        self.v  = np.multiply(self.vd,dt)+self.v
+        self.x = np.multiply(self.xd,dt)+self.x
+        self.v = np.multiply(self.vd,dt)+self.v
         
-        self.z  = np.multiply(self.zd,dt)+self.z
-        self.y  = np.multiply(yd,dt)+self.y
+        self.z = np.multiply(self.zd,dt)+self.z
+        self.y = np.multiply(yd,dt)+self.y
         
-        self.g  = np.multiply(self.gd,dt)+self.g
+        self.g = np.multiply(self.gd,dt)+self.g
         
-        #Return o/p trajectory
+        # Return o/p trajectory
         y = self.y
         return y,yd,ydd
 
+    """
+        Changes the start position of the DMP
+            Inputs:
+                start_val: start position
+    """
     def setStart(self, start_val):
-       self.y = start_val
+        self.y = start_val
 
-       # Changes the goal position of the DMP
-       # Inputs:
-       #    G   : goal position
-       #    flag: 1 initial setting; 0 for mid-run update
-
+    """
+        Changes the goal position of the DMP
+            Inputs:
+                G   : goal position
+                flag: 1 initial setting; 0 mid-run update
+    """
     def setGoal(self, goal_val, flag):
        self.G = goal_val
 
@@ -98,13 +108,21 @@ class DMP_runner():
                # dG based scaling cab work automatically
                self.s = np.divide((self.G - self.y0), float(self.dG))
 
-
-#Reads in the XML file
-        
+"""
+    Parses XML file containing trained DMP data.  Current format is:
+        <root>
+            <Weights>...</Weights>
+            <inv_sq_var>...</inv_sq_var>
+            <gauss_means>...</gauss_means>
+            <dD>...</dG>
+            <A>...</A>
+            <s>...</s>
+        </root>
+"""
 def readInXML(runner, filename):
     runner.w = []
     runner.D = []
-    runner.c= []
+    runner.c = []
     
     root = ET.parse(filename).getroot()
 
